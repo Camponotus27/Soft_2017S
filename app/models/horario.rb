e1311f00f495555a4ff52ee285abc16f6522653c
marcelo <espinoza.marcelo@live.com>
2017-12-10 16:40:02 -0300

    reparaciones de reserva de hora

--- app/models/horario.rb
+++ app/models/horario.rb
@@ -3,5 +3,5 @@ class Horario < ApplicationRecord
   validates :fecha, date: { after: Proc.new { Time.now }}
 
   validates :fecha, uniqueness: { scope: :profesional,  message: "horario ya ingresado" }
-
+  validates :profesional, presence: true, :length => { :minimum => 5, message: "horario ya ingresado"}
 end
